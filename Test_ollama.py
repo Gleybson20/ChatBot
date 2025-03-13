e60ed78eae31e9210caf212fa4bf5dc538583624
gleybson20 <gleybson.ricardo20@gmail.com>
2025-03-13 15:09:23 -0300

    Deletando arquivos desnecessários

--- Test_ollama.py
+++
@@ -1,8 +0,0 @@
-import ollama
-
-response = ollama.chat(
-    model="mistral",
-    messages=[{"role": "user", "content": "O que é a A.C.E. Consultoria?"}]
-)
-
-print(response["message"]["content"])
